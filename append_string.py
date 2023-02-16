e1a751ba9fc8aa291a17dcd6ed91ddce41b616ff
DavidScann <53106687+DavidScann@users.noreply.github.com>
2023-02-16 11:48:41 -0700

    Update append_string.py

--- append_string.py
+++ append_string.py
@@ -3,38 +3,32 @@
 
 import os
 import sys
-import fileinput
-import re
-import multiprocessing
 
-# Define a regular expression to modify the string format
-re_format = re.compile(r"[_!,]")
+def append_string_to_files(dir_path, appended_string):
+    # Split the appended string into separate phrases
+    # separated by spaces, then replace underscores with spaces
+    # and join the phrases with comma-spaces
+    formatted_string = ", ".join([phrase.replace("_", " ") for phrase in appended_string.split()])
 
-# Define a worker function to modify the file
-def modify_file(file_path, modified_string):
-    for line in fileinput.input(file_path, inplace=True):
-        print(line.rstrip() + ", " + modified_string)
+    # Walk through the directory, find all the .txt files, and
+    # append the formatted string to the end of each file
+    for root, _, files in os.walk(dir_path):
+        for file_name in files:
+            if file_name.endswith(".txt"):
+                file_path = os.path.join(root, file_name)
+                # Use a with statement to ensure the file is closed after writing
+                with open(file_path, "a", encoding="utf-8") as file:
+                    file.write(", " + formatted_string)
 
-# Get the directory and appended string from command-line arguments
-if len(sys.argv) != 3:
-    print("Usage: python append_string.py <directory> <appended_string>")
-    sys.exit(1)
-
-directory = sys.argv[1]
-appended_string = sys.argv[2]
-
-# Modify the appended string format
-modified_string = re_format.sub(lambda m: " " if m.group(0) == "_" else "", appended_string) + ", "
-
-# Create a pool of worker processes
-pool = multiprocessing.Pool()
-
-# Loop through all the files in the directory
-for file in os.scandir(directory):
-    if file.name.endswith(".txt") and file.is_file():
-        # Schedule the file for processing by a worker process
-        pool.apply_async(modify_file, args=(file.path, modified_string))
-
-# Wait for all worker processes to finish
-pool.close()
-pool.join()
+if __name__ == "__main__":
+    # Check if the user provided the directory path and appended string as arguments
+    if len(sys.argv) == 3:
+        # Call the function with the provided directory path and appended string
+        append_string_to_files(sys.argv[1], sys.argv[2])
+    # Check if the user provided only the appended string as an argument
+    elif len(sys.argv) == 2:
+        # Call the function with the current directory and the provided appended string
+        append_string_to_files(".", sys.argv[1])
+    else:
+        # If the user did not provide the correct number of arguments, print usage instructions
+        print("Usage: python script_name.py [dir_path] appended_string")
