d2bae0c7f9ad91439e7f018c7abf80cb77fbfad4
DavidScann <53106687+DavidScann@users.noreply.github.com>
2023-02-16 02:13:24 -0700

    Update append_string.py

--- append_string.py
+++ append_string.py
@@ -3,26 +3,38 @@
 
 import os
 import sys
+import fileinput
+import re
+import multiprocessing
 
-def process_file(file_path, tags):
-    with open(file_path, 'r', encoding='utf-8') as f:
-        lines = f.readlines()
+# Define a regular expression to modify the string format
+re_format = re.compile(r"[_!,]")
 
-    with open(file_path, 'w', encoding='utf-8') as f:
-        for line in lines:
-            line = line.strip().replace('_', ' ').replace(' ', ', ')
-            f.write(line + ', ' + tags + '\n')
+# Define a worker function to modify the file
+def modify_file(file_path, modified_string):
+    for line in fileinput.input(file_path, inplace=True):
+        print(line.rstrip() + ", " + modified_string)
 
-def main():
-    folder_path = sys.argv[1]
-    tags = sys.argv[2]
+# Get the directory and appended string from command-line arguments
+if len(sys.argv) != 3:
+    print("Usage: python append_string.py <directory> <appended_string>")
+    sys.exit(1)
 
-    for root, dirs, files in os.walk(folder_path):
-        for file in files:
-            if file.endswith('.txt'):
-                file_path = os.path.join(root, file)
-                process_file(file_path, tags)
-                print(f'{file} has been updated')
+directory = sys.argv[1]
+appended_string = sys.argv[2]
 
-if __name__ == '__main__':
-    main()
+# Modify the appended string format
+modified_string = re_format.sub(lambda m: " " if m.group(0) == "_" else "", appended_string) + ", "
+
+# Create a pool of worker processes
+pool = multiprocessing.Pool()
+
+# Loop through all the files in the directory
+for file in os.scandir(directory):
+    if file.name.endswith(".txt") and file.is_file():
+        # Schedule the file for processing by a worker process
+        pool.apply_async(modify_file, args=(file.path, modified_string))
+
+# Wait for all worker processes to finish
+pool.close()
+pool.join()
