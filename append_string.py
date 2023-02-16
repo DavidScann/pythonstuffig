4e9c821981bd5fac1b157f8c85b53070240066fa
DavidScann <53106687+DavidScann@users.noreply.github.com>
2023-02-16 01:17:47 -0700

    Update append_string.py

--- append_string.py
+++ append_string.py
@@ -4,40 +4,25 @@
 import os
 import sys
 
-def format_tags(tag_list):
-    # Replace underscores with spaces
-    tag_list = [tag.replace('_', ' ') for tag in tag_list]
+def process_file(file_path, tags):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
 
-    # Join the items in the list with a comma separator
-    return ', '.join(tag_list)
+    with open(file_path, 'w', encoding='utf-8') as f:
+        for line in lines:
+            line = line.strip().replace('_', ' ').replace(' ', ', ')
+            f.write(line + ', ' + tags + '\n')
 
-# Check if the user provided a folder path as a command line argument
-if len(sys.argv) < 3:
-    # Print instructions on how to use the script
-    print("Please provide a folder path and a string to append, separated by a space.")
-    print("Usage: python append_string.py /path/to/folder/ 'tags'")
-else:
-    # Get the folder path and string to append from the command line arguments
+def main():
     folder_path = sys.argv[1]
-    append_string = sys.argv[2].replace('_', ', ')
+    tags = sys.argv[2]
 
-    # Loop through every file in the folder
-    for filename in os.listdir(folder_path):
-        if filename.endswith('.txt'):
-            # Construct the full file path
-            file_path = os.path.join(folder_path, filename)
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith('.txt'):
+                file_path = os.path.join(root, file)
+                process_file(file_path, tags)
+                print(f'{file} has been updated')
 
-            # Open the file
-            with open(file_path, 'r') as file:
-                # Read the contents of the file into a list
-                items = [line.strip() for line in file]
-
-            # Format the tags
-            output = format_tags(items)
-
-            # Append the string to the output
-            output += ', ' + append_string
-
-            # Overwrite the file with the output
-            with open(file_path, 'w') as file:
-                file.write(output)
+if __name__ == '__main__':
+    main()
